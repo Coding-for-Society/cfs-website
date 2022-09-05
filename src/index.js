b639cb981cd918c780086fc7b27bb3b0fe875c17
Jonas <jonas.itt@tum.de>
2022-09-05 11:48:32 +0200

    Addded glitch effect

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/* Reacts strict mode renders components twice when developing to display warnings */
+/* It doesn't do that in that built app */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
