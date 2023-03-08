a8aefb30d6a3dbc8bb349556d77c204bae2c7576
JI20 <81356927+JI20@users.noreply.github.com>
2023-03-08 14:08:06 +0100

    Add Routing (#4)
    * Clean up project and add ESLint
    
    * Change ESLint rule: quotes
    
    * Change ESLint and add dev routes for single pages

---
+++ src/Routes.jsx
@@ -0,0 +1,76 @@
+import {
+  createBrowserRouter,
+  Link,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+export default function Routes() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate replace to="/routes" />,
+    },
+    {
+      path: "/routes",
+      element: (
+        <>
+          <h1>Hi, choose your path here!</h1>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+            <li>
+              <Link to="/goals">Goals</Link>
+            </li>
+            <li>
+              <Link to="/apply">Apply</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </>
+      ),
+      children: [],
+    },
+    {
+      path: "/home",
+      element: <h1>Home</h1>,
+      children: [],
+    },
+    {
+      path: "/projects",
+      element: <h1>projects</h1>,
+      children: [],
+    },
+    {
+      path: "/team",
+      element: <h1>team</h1>,
+      children: [],
+    },
+    {
+      path: "/goals",
+      element: <h1>goals</h1>,
+      children: [],
+    },
+    {
+      path: "/apply",
+      element: <h1>Apply</h1>,
+      children: [],
+    },
+    {
+      path: "/contact",
+      element: <h1>Contact</h1>,
+      children: [],
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
+}
