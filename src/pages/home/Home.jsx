f152e2a8a35df78afc60cdaf406274496bcefd7a
JI20 <jonas.itt@tum.de>
2023-03-09 16:08:03 +0100

    Postion Bubbles and Texts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,47 +9,60 @@ function Home() {
   return (
     <div className="homeContainer">
       <div className="sloganContainer">
-        <h1>
-          We develop <span id="Software">software</span>
-          <br />
-          for the good cause
-        </h1>
-        <SloganBubble />
+        <div className="sloganText">
+          <h1>
+            We develop <span id="Software">software</span>
+            <br />
+            for the good cause
+          </h1>
+        </div>
+        <SloganBubble className="sloganBubble" />
       </div>
       <div className="motivationContainer">
-        <h2>Our motivation</h2>
-        <p>
-          As a student initiative Coding for Society aims for positive societal
-          impact through creating software projects with social value. Together
-          as a motivated and interdisciplinary team of international students at
-          the Technical University of Munich we work on projects that take on
-          all shapes and colors.
-        </p>
-        <MotivationBubble />
+        <div className="motivationText">
+          <h2>Our motivation</h2>
+          <p>
+            As a student initiative Coding for Society aims for positive
+            societal impact through creating software projects with social
+            value. Together as a motivated and interdisciplinary team of
+            international students at the Technical University of Munich we work
+            on projects that take on all shapes and colors.
+          </p>
+        </div>
+
+        <MotivationBubble className="motivationBubble" />
       </div>
       <div className="projectsContainer">
-        <h2>Our latest projects</h2>
-        <p>
-          All of our projects focus on providing social value. We work together
-          with other social teams and realize our own ideas.
-        </p>
-        <ProjectBubble />
+        <div className="projectsText">
+          <h2>Our latest projects</h2>
+          <p>
+            All of our projects focus on providing social value. We work
+            together with other social teams and realize our own ideas.
+          </p>
+        </div>
+
+        <ProjectBubble className="projectsBubble" />
       </div>
       <div className="teamContainer">
-        <h2>Our Team</h2>
-        <p>
-          We are an interdisciplinary team of committed students of the
-          Technical University of Munich (TUM).
-        </p>
-        <TeamBubble />
+        <div className="teamText">
+          <h2>Our Team</h2>
+          <p>
+            We are an interdisciplinary team of committed students of the
+            Technical University of Munich (TUM).
+          </p>
+        </div>
+        <TeamBubble className="teamBubble" />
       </div>
       <div className="contactContainer">
-        <h2>Reach us</h2>
-        <p>
-          Feel free to contact us! We are always open to new project ideas,
-          feedback or support.
-        </p>
-        <ContactBubble />
+        <div className="contactText">
+          <h2>Reach us</h2>
+          <p>
+            Feel free to contact us! We are always open to new project ideas,
+            feedback or support.
+          </p>
+        </div>
+
+        <ContactBubble className="contactBubble" />
       </div>
     </div>
   );
