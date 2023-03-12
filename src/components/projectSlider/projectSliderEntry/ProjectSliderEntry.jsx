12e06eadb67ea50d6e6d6ba728bb7d74191012d2
JI20 <jonas.itt@tum.de>
2023-03-12 16:23:17 +0100

    Make Project Slider Entries clickable

--- src/components/projectSlider/projectSliderEntry/ProjectSliderEntry.jsx
+++ src/components/projectSlider/projectSliderEntry/ProjectSliderEntry.jsx
@@ -1,13 +1,24 @@
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ProjectSliderEntry(props) {
   const { project } = props;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/projects/${project.id}`);
+  };
 
   return (
-    <div className="projectSliderEntry">
+    <div
+      className="projectSliderEntry"
+      onClick={handleNavigate}
+      onKeyUp={handleNavigate}
+      role="button"
+      tabIndex={0}
+    >
       <img src={project.image} alt={project.name} />
       <h3 className="projectSliderEntryText">{project.name}</h3>
-      <p>{project.desc}</p>
     </div>
   );
 }
