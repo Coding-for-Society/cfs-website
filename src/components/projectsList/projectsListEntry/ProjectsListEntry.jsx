cc2fda172dfce04947500af5f9c0dfd1f239d9bf
JI20 <jonas.itt@tum.de>
2023-04-01 14:39:07 +0200

    Add styling of project cards

--- src/components/projectsList/projectsListEntry/ProjectsListEntry.jsx
+++ src/components/projectsList/projectsListEntry/ProjectsListEntry.jsx
@@ -2,5 +2,15 @@ import "./style.css";
 
 export default function ProjectsListEntry(props) {
   const { project } = props;
-  return <div>{project.name}</div>;
+  return (
+    <div className="projectsListEntry">
+      <div className="imageOverlay">Test</div>
+      <div
+        style={{ backgroundImage: `url(${project.image})` }}
+        alt={project.name}
+        className="projectsListEntryImg"
+      />
+      <h3 className="projectListEntryText">{project.name}</h3>
+    </div>
+  );
 }
