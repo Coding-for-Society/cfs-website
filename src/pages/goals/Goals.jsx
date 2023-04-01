6292e92a39d0f299ac3c87537f4f15eaf0d3e683
Kai <>
2023-04-01 19:44:23 +0200

    Implementation of goals page using scrolltrigger in progress

--- src/pages/goals/Goals.jsx
+++ src/pages/goals/Goals.jsx
@@ -1,24 +1,57 @@
-import LiquidSwipe from "react-liquidswipe";
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import components from "./config";
+import GoalsBubble from "./bubbles/GoalsBubble";
 import "./style.css";
 
-/* change bgColor when the component changes */
 function Goals() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    components.forEach((component, index) => {
+      const trigger = index === 0 ? ".goalsTitle" : `#component-${index}`;
+      const start = index === 0 ? "top center" : `top+=${index * 100} center`;
+      const end = index === components.length - 1 ? "bottom center" : `top+=${(index + 1) * 100} center`;
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger,
+          start,
+          end,
+          scrub: true,
+          toggleActions: "play none none reverse",
+        },
+      })
+        .from(`#component-${index}`, { opacity: 0, y: 100 })
+        .to(`#component-${index}`, { opacity: 0, y: -100 });
+    });
+  }, []);
+
   return (
     <div className="goalsContainer">
-      <h1>Our Goals</h1>
+      <h1 className="goalsTitle">Our Goals</h1>
       <div className="blockquote">
         <p className="blockquoteText">
-          &quot;By aligning our mission with the values and expectations of society and the needs of
-          the natural world, we are creating the basis for trustworthy, socially acceptable and
-          sustainable innovations.&quot;
+          &quot;Our mission at Coding for Society is to make use of our computer science skills
+          for what we believe to be the best purpose possible: to create social value. <br />
+          We commit to making the world a better place.&quot;
         </p>
         <p className="blockquoteAuthor">Jonas Itt</p>
         <p className="blockquoteAuthor2">Founder of Coding for Society</p>
       </div>
-      <div className="liquidSwipe">
-        <LiquidSwipe components={components} style={{ height: '70vh', borderRadius: '20px' }} />
+      <div className="goalsTextContainer">
+        <GoalsBubble
+          className="goalsBubble"
+          preserveAspectRatio="none"
+        />
+        {components.map((component) => (
+          <div className="goalsText" id={component.id}>
+            <h3>{component.title}</h3>
+            {component.text}
+          </div>
+        ))}
       </div>
+      <div className="spacer" />
     </div>
   );
 }
--- src/pages/goals/config.jsx
+++ src/pages/goals/config.jsx
@@ -1,31 +1,37 @@
 const components = [
-  <div className="goalsText goalsText1">
-    <h3>Social Value</h3>
-    <p>
-      At Coding for Society, we aim to make a positive social impact by working on
-      software projects that are beneficial to everyone.<br />
-      As a group of motivated students from various disciplines and countries, we work together on
-      our projects.
-    </p>
-  </div>,
-  <div className="goalsText goalsText2">
-    <h3>Education</h3>
-    <p>
-      Education is key to a successful future.<br />
-      This is why we are working on projects to get children excited about coding and to help them
-      learn the basics.
-      At the same time we are also sharing knowledge between our members to learn from each other
-      and improve our skills.
-    </p>
-  </div>,
-  <div className="goalsText goalsText3">
-    <h3>Accessibility</h3>
-    <p>
-      With our projects, we aim to make technology and software in particular accessible to
-      everyone.<br />
-      This is also why all of our projects are open source.
-    </p>
-  </div>,
+  {
+    title: "Social Value",
+    text: (
+      <p>
+        At Coding for Society, we aim to make a positive social impact by working on
+        software projects that are beneficial to everyone.<br />
+        As a group of motivated students from various disciplines and countries, we work together on
+        our projects.
+      </p>
+    ),
+  },
+  {
+    title: "Education",
+    text: (
+      <p>
+        Education is key to a successful future.<br />
+        This is why we are working on projects to get children excited about coding and to help them
+        learn the basics.
+        At the same time we are also sharing knowledge between our members to learn from each other
+        and improve our skills.
+      </p>
+    ),
+  },
+  {
+    title: "Accessibility",
+    text: (
+      <p>
+        With our projects, we aim to make technology and software in particular accessible to
+        everyone.<br />
+        This is also why all of our projects are open source.
+      </p>
+    ),
+  },
 ];
 
 export default components;
