0b9721ded11258d76b58abfb451e7043fa1fa4f8
JI20 <jonas.itt@tum.de>
2023-04-01 13:26:57 +0200

    Add Team Detail Page

--- src/pages/team/teamDetail/TeamDetail.jsx
+++ src/pages/team/teamDetail/TeamDetail.jsx
@@ -1,19 +1,35 @@
 import { useParams } from "react-router-dom";
+import TeamDetailBubble from "./bubbles/TeamDetailBubble";
+import { teamConfig } from "../../../config";
+import "./style.css";
 
 export default function TeamDetail() {
   const { id } = useParams();
 
+  const member = Object.values(teamConfig).find((m) => m.id === id);
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        top: 0,
-        left: 0,
-        backgroundColor: "red",
-      }}
-    >
-      {id}
+    <div className="teamDetailContainer">
+      <h1 className="teamDetailHeading">Our Team</h1>
+
+      <div className="teamDetailContentContainer">
+        <div className="teamDetailContent">
+          <h2>{member.name}</h2>
+          <div className="teamDetailGrid">
+            <div className="teamDetailImageContainer">
+              <img src={member.image} alt={member.name} />
+            </div>
+            <div className="teamDetailGridText">
+              <h3>{member.role}</h3>
+              <p>{member.own_text}</p>
+            </div>
+          </div>
+        </div>
+        <TeamDetailBubble
+          className="teamDetailBubble"
+          preserveAspectRatio="none"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/pages/team/teamDetail/bubbles/TeamDetailBubble.jsx
@@ -0,0 +1,17 @@
+const TeamDetailBubble = (props) => (
+  <svg
+    width={1920}
+    height={1361}
+    viewBox="0 0 1920 1361"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="#01001D"
+      d="M0 103.695C137.5 114 150 42.21 246.5 17c245-64.004 321.5 76.935 545 121.597 317 63.346 395.88 40.352 599.5 0L1625.5 80.5c160-29.5 250.19-11.31 294.5 23.195V1361H0V103.695z"
+    />
+  </svg>
+);
+
+export default TeamDetailBubble;
