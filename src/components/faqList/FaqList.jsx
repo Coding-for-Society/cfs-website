5d58f4dd8df9ee10f0580712c1af7791e06a3453
Kai <>
2023-06-01 19:33:50 +0200

    modifications to apply page: make multiple FAQs expandable, reduce space

--- src/components/faqList/FaqList.jsx
+++ src/components/faqList/FaqList.jsx
@@ -3,28 +3,33 @@ import ExpandIcon from "./icons/ExpandIcon";
 import './style.css';
 
 const FaqList = ({ faqConfig }) => {
-  const [expandedQuestion, setExpandedQuestion] = useState(null);
+  const [expandedQuestions, setExpandedQuestions] = useState([]);
 
   const handleQuestionClick = (id) => {
-    setExpandedQuestion((prev) => (prev === id ? null : id));
+    setExpandedQuestions((prev) => {
+      if (prev.includes(id)) {
+        return prev.filter((questionId) => questionId !== id);
+      }
+      return [...prev, id];
+    });
   };
 
   return (
     <div className="faqList">
-      {faqConfig.map((faq, index) => (
+      {faqConfig.map((faq) => (
         <div key={faq.id}>
           <h3>
             <a href={`#${faq.id}`} onClick={() => handleQuestionClick(faq.id)}>
-              <ExpandIcon expanded={expandedQuestion === faq.id} className="expandIcon" />
+              <ExpandIcon expanded={expandedQuestions.includes(faq.id)} className="expandIcon" />
               {faq.question}
             </a>
           </h3>
-          {expandedQuestion === faq.id && (
+          {expandedQuestions.includes(faq.id) && (
             <div>
               <p>{faq.answer}</p>
             </div>
           )}
-          {index !== faqConfig.length - 1 && <hr />}
+          <hr />
         </div>
       ))}
     </div>
