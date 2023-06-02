13e3ce8e9e3cbb2f0687af1687cb795a9fa230e1
Jakob <31383697+Schabolon@users.noreply.github.com>
2023-06-02 16:10:28 +0200

    implement snapping

--- src/components/teamSlider/TeamSlider.jsx
+++ src/components/teamSlider/TeamSlider.jsx
@@ -1,41 +1,35 @@
+/* eslint-disable */
 import "./style.css";
-import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
 import TeamSliderEntry from "./teamSliderEntry/teamSliderEntry";
+import ScrollDrag from "./ScrollDrag";
+
+function sayHello() {
+  const element = document.getElementById('team-slider');
+  element.scrollBy({
+    top: 0,
+    left: 1000,
+    behavior: "smooth",
+  });
+}
 
 export default function TeamSlider(props) {
   const { team } = props;
-  const [sliderWidth, setSliderWidth] = useState(0);
-  const teamSlider = useRef();
-
-  useEffect(() => {
-    setSliderWidth(
-      teamSlider.current.scrollWidth - teamSlider.current.offsetWidth,
-    );
-  }, []);
 
   return (
-    <motion.div
-      ref={teamSlider}
-      className="outerTeamSlider"
-      whileTap={{ cursor: "grabbing" }}
+    <div
+      id="team-slider"
+      className="teamSliderEntryList"
     >
-      <motion.div
-        drag="x"
-        dragConstraints={{ right: 0, left: -sliderWidth }}
-        className="teamSlider"
-      >
-        <ul className="teamSliderEntryList">
-          {Object.values(team).map((member, i, { length }) => (
-            <li
-              key={member.id}
-              className={i + 1 === length ? "lastTeamSliderItem" : ""}
-            >
-              <TeamSliderEntry member={member} />
-            </li>
-          ))}
-        </ul>
-      </motion.div>
-    </motion.div>
+      <button type="button" onClick={sayHello}>Default</button>;
+      {Object.values(team).map((member, i, { length }) => (
+        <div
+          key={member.id}
+          className={i + 1 === length ? "lastTeamSliderItem" : ""}
+        >
+          <TeamSliderEntry member={member} />
+        </div>
+      ))}
+    </div>
   );
 }
+/* eslint-disable */
