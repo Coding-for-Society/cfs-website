7ebc652eb771f8d8f3782d66ac15c2fbb1d339fe
JI20 <jonas.itt@tum.de>
2023-06-05 15:26:28 +0200

    Add react responsiveness

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -7,10 +7,25 @@ import navConfig from "./config";
 export default function NavigationBar() {
   const location = useLocation();
   const [currentPage, setCurrentPage] = useState("/home");
+  const [open, setOpen] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+  });
 
   useEffect(() => {
     setCurrentPage(location.pathname);
   }, [location]);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className="navBar">
@@ -19,23 +34,32 @@ export default function NavigationBar() {
           <h1>Coding for Society.</h1>
         </Link>
       </div>
-      <div className="navBarLinkContainer">
-        <ul className="navBarLinks">
-          {navConfig.map((navLink) => (
-            <li key={navLink}>
-              <Link
-                className={
-                  currentPage.startsWith(navLink.path) ? "activeLink" : ""
-                }
-                to={navLink.path}
-              >
-                {navLink.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <NavBarBubble className="navBarBubble" />
-      </div>
+      {width < 800 ? (
+        <div>
+          <button type="button" onClick={toggleOpen}>
+            Open
+          </button>
+          {open && <h1 style={{ color: "white" }}>Yaaaaaay!</h1>}
+        </div>
+      ) : (
+        <div className="navBarLinkContainer">
+          <ul className="navBarLinks">
+            {navConfig.map((navLink) => (
+              <li key={navLink}>
+                <Link
+                  className={
+                    currentPage.startsWith(navLink.path) ? "activeLink" : ""
+                  }
+                  to={navLink.path}
+                >
+                  {navLink.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <NavBarBubble className="navBarBubble" />
+        </div>
+      )}
     </div>
   );
 }
