6b88f3d03432f8571111c1d63a8ffbd338f56a43
JI20 <jonas.itt@tum.de>
2023-06-08 16:18:34 +0200

    Add support for height to viewport

--- src/hooks/useViewport.jsx
+++ src/hooks/useViewport.jsx
@@ -3,20 +3,22 @@ import { useEffect, useState } from "react";
 const useViewport = () => {
   // Width State
   const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
   // Registering resize event listener on mounting
   useEffect(() => {
-    const changeWidth = () => {
+    const handleResize = () => {
       setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
     };
-    window.addEventListener("resize", changeWidth);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("resize", changeWidth);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  return { width };
+  return { width, height };
 };
 
-export useViewport;
+export default useViewport;
