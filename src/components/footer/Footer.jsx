30692b9284358b173cea126008e19b245f607098
Kai <>
2023-06-17 13:39:50 +0200

    make footer responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import FooterBubble from "./FooterBubble";
+import { useViewport } from "../../context/viewportContext";
 import "./style.css";
 
 function Footer() {
+  const { width } = useViewport();
+
   return (
     <footer>
       <div className="footerColumns">
@@ -13,30 +16,34 @@ function Footer() {
             <Link to="/contact" className="contactLink">Contact</Link>
           </div>
         </div>
-        <div className="footerColumn">
-          <h3 className="linksText">Links</h3>
-          <ul>
-            <li><Link to="/projects" className="projectsLink">Our Projects</Link></li>
-            <li><Link to="/team" className="teamLink">Our Team</Link></li>
-            <li><Link to="/goals" className="goalsLink">Our Goals</Link></li>
-          </ul>
-        </div>
-        <div className="footerColumn">
-          <h3 className="contactText">Contact</h3>
-          <ul>
-            <li><Link to="mailto:hello@codingforsociety.com" className="mailLink">E-Mail</Link></li>
-            <li><Link to="https://www.instagram.com" className="instagramLink">Instagram</Link></li>
-            <li><Link to="https://www.linkedin.com/company/coding-for-society/" className="linkedinLink">Linkedin</Link></li>
-          </ul>
-        </div>
-        <div className="footerColumn">
-          <h3 className="otherText">Other</h3>
-          <ul>
-            <li><Link to="https://www.tum.de/studium/studienfinanzierung/stipendien/stipendien-der-tum/deutschlandstipendium/was-wir-machen/wir-engagieren-uns/" className="tsLink">Talente Spenden</Link></li>
-            <li><Link to="https://www.tum.de/studium/studienfinanzierung/stipendien/stipendien-der-tum/deutschlandstipendium/" className="dlsLink">TUM Deutschlandstipendium</Link></li>
-            <li><Link to="/imprint" className="imprintLink">Imprint</Link></li>
-          </ul>
-        </div>
+        {width > 1080 && (
+          <>
+            <div className="footerColumn">
+              <h3 className="linksText">Links</h3>
+              <ul>
+                <li><Link to="/projects" className="projectsLink">Our Projects</Link></li>
+                <li><Link to="/team" className="teamLink">Our Team</Link></li>
+                <li><Link to="/goals" className="goalsLink">Our Goals</Link></li>
+              </ul>
+            </div>
+            <div className="footerColumn">
+              <h3 className="contactText">Contact</h3>
+              <ul>
+                <li><Link to="mailto:hello@codingforsociety.com" className="mailLink">E-Mail</Link></li>
+                <li><Link to="https://www.instagram.com" className="instagramLink">Instagram</Link></li>
+                <li><Link to="https://www.linkedin.com/company/coding-for-society/" className="linkedinLink">Linkedin</Link></li>
+              </ul>
+            </div>
+            <div className="footerColumn">
+              <h3 className="otherText">Other</h3>
+              <ul>
+                <li><Link to="https://www.tum.de/studium/studienfinanzierung/stipendien/stipendien-der-tum/deutschlandstipendium/was-wir-machen/wir-engagieren-uns/" className="tsLink">Talente Spenden</Link></li>
+                <li><Link to="https://www.tum.de/studium/studienfinanzierung/stipendien/stipendien-der-tum/deutschlandstipendium/" className="dlsLink">TUM Deutschlandstipendium</Link></li>
+                <li><Link to="/imprint" className="imprintLink">Imprint</Link></li>
+              </ul>
+            </div>
+          </>
+        )}
       </div>
       <FooterBubble className="footerBubble" preserveAspectRatio="none" />
     </footer>
